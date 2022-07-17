d866ce42237553090f39d252596d1a06435deb88
priscila-tome <priscilatome.santos2000@gmail.com>
2022-07-17 12:47:52 -0300

    Atualização formulários

--- formularios.js
+++ formularios.js
@@ -1,14 +1,18 @@
+
 const btnVol = window.document.getElementById("btn_vol")
 const btnInst = window.document.getElementById("btn_inst")
 const formVol = window.document.getElementById("form_vol")
 const formInst = window.document.getElementById("form_inst")
+const ladoEsq = window.document.querySelector(".lado-esquerdo")
 
 function forminst() {
+    ladoEsq.classList.add("on")
     formVol.classList.add("off")
     formInst.classList.remove("off")
 }
 
 function formvol() {
+    ladoEsq.classList.add("on")
     formVol.classList.remove("off")
     formInst.classList.add("off")
 }
