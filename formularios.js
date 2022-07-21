3eb5a711edeb5c50498373305328faba6083c2a0
priscila-tome <priscilatome.santos2000@gmail.com>
2022-07-20 22:10:25 -0300

    Correção JS

--- formularios.js
+++ formularios.js
@@ -1,17 +1,17 @@
-
-const btnVol = window.document.getElementById("btn_vol")
-const btnInst = window.document.getElementById("btn_inst")
 const formVol = window.document.getElementById("form_vol")
 const formInst = window.document.getElementById("form_inst")
 const ladoEsq = window.document.querySelector(".lado-esquerdo")
+const ladoDir = window.document.querySelector(".lado-direito")
 
 function forminst() {
+    ladoDir.classList.remove("off")
     ladoEsq.classList.add("on")
     formVol.classList.add("off")
     formInst.classList.remove("off")
 }
 
 function formvol() {
+    ladoDir.classList.remove("off")
     ladoEsq.classList.add("on")
     formVol.classList.remove("off")
     formInst.classList.add("off")
